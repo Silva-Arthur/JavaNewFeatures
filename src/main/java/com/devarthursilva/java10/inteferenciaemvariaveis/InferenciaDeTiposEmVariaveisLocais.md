36a2f32720084ff0afafec1da2164d73de56852d
Arthur <pereiras.oarthur@gmail.com>
2023-11-29 14:59:50 -0300

    Java 10 - Inferencia de tipos em variáveis locais

---
+++ src/main/java/com/devarthursilva/java10/inteferenciaemvariaveis/InferenciaDeTiposEmVariaveisLocais.md
@@ -0,0 +1,15 @@
+# Definição
+
+Criar inferencia de tipos em variáveis locais, sem a necessidade de definir o tipo de variável
+
+- Variável deve ser declarada como local e inicializada
+- Pode ser usada no Enhanced For Loop
+- Pode ser usada como indice no laço For tradicional
+
+# Sintaxe
+```
+var // uso da palavra reservada var antes do nome da variavél
+
+var i = 0;
+var nome = "Arthur";
+```
---
+++ src/main/java/com/devarthursilva/java10/inteferenciaemvariaveis/InferenciaDeTiposEmVariaveisLocaisExemplo.java
@@ -0,0 +1,31 @@
+package com.devarthursilva.java10.inteferenciaemvariaveis;
+
+import java.util.List;
+
+public class InferenciaDeTiposEmVariaveisLocaisExemplo {
+	
+	public static void main(String[] args) {
+		
+		var a1 = "Arthur Silva";
+		
+		String a2 = "Programador";
+		
+		System.out.println(a1 + " " + a2);
+		
+		System.out.println("==========");
+		
+		var listaDeFrutas = List.of("banana", "maçã", "abacaxi");
+		
+		//enhanced for
+		for(var fruta : listaDeFrutas) {
+			System.out.println(fruta);
+		}
+		
+		System.out.println("==========");
+		
+		// for tradicional
+		for(var i = 0; i < listaDeFrutas.size(); i++) {
+			System.out.println(listaDeFrutas.get(i));
+		}
+	}
+}
