51413f14c18eba393a4bffc9ce312f13840f635b
Arthur <pereiras.oarthur@gmail.com>
2023-11-29 23:35:17 -0300

    Java 14 - TextBlocks

---
+++ src/main/java/com/devarthursilva/java14/textblocks/TextBlocks.java
@@ -0,0 +1,22 @@
+package com.devarthursilva.java14.textblocks;
+
+public class TextBlocks {
+	
+	public static void main(String[] args) {
+		/**
+		 * 
+		 * Adiciona o \s para adioconar espaços entre as palavras
+		 * Adiciona o \ para quebra linhas que sao escritas dentro das aspas triplas serem escritas como uma linha só
+		 * 
+		 * */
+		String texto = 
+				"""
+				Olá\s\s\s Mundo!
+				Este é o módulo Java 14. \
+				\
+				Dev Arthur Silva
+				""";
+		
+		System.out.println(texto);
+	}
+}
